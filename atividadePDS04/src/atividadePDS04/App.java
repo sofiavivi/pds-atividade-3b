09c1350dff08dcfceacfe7fd8678653c14d1504f
Sofia Vitória Barbosa da Fonseca <20211064010028@ifrn.local>
2024-12-16 18:09:38 -0300

    Projeto pronto

---
+++ atividadePDS04/src/atividadePDS04/App.java
@@ -0,0 +1,14 @@
+package atividadePDS04;
+
+public class App {
+
+	public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa1 = new Pessoa("Sofia", 19);
+
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+        pessoa1.exibirInformacoes();
+    }
+
+}
